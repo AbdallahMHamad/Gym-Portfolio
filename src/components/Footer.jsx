fe732147155b2d62995b370fd78854eebfb175cf
Abdallah <Aboodghannam0@gmail.com>
2026-03-05 01:24:05 +0300

    foot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,12 +10,14 @@ function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
-          <p className="rights-text">
-            © {currentYear} <strong>Coach Salman</strong>. {t('footer_rights')}
-          </p>
           <p className="made-by">
             {t('footer_made_by')} <span className="heart">🔥</span> <a href="https://www.instagram.com/abdallah.learns/" target="_blank" rel="noreferrer">Abdallah Hamad</a>
           </p>
+
+                    <p className="rights-text">
+            © {currentYear} <strong>Coach Salman</strong>. {t('footer_rights')}
+          </p>
+
         </div>
       </div>
     </footer>
